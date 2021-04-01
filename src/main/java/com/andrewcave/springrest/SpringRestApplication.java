affb0cbb7dec05dc4a14eefefbbc01e6c4cc141a
Andrew Cave <andrew.j.cave@gmail.com>
2021-04-02 08:54:57 +1000

    Change package name

--- src/main/java/com/andrewcave/springrest/SpringRestApplication.java
+++ src/main/java/com/andrewcave/springrest/SpringRestApplication.java
@@ -1,4 +1,4 @@
-package com.andrewcave.springrest;
+package com.therealcavey.springrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
